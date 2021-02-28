3d363819076db36ce2a7cd713da5ce1e60a7dbdc
Carlos Silva <carlosrique13@hotmail.com>
2021-02-28 15:58:14 -0400

    Done

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,13 +1,29 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
-
+import * as yup from 'yup'
+import { AppError } from "../errors/AppError";
 
 class UserController {
 
   async create(request: Request, response: Response) {
     const {name, email} = request.body;
+
+    const schema = yup.object().shape({
+      name: yup.string().required("Your name is required man"),
+      email: yup.string().email().required("Your email is required man")
+    });
+
+    // if(!(await schema.isValid(request.body))) {
+    //   return response.status(400).json({error: "Validation Failed!"});
+    // }
     
+    try {
+      await schema.validate(request.body, { abortEarly: false})
+    } catch (error) {
+      throw new AppError(error)
+    }
+
     const usersRepository = getCustomRepository(UsersRepository)
 
     const userAlreadyExists = await usersRepository.findOne({
@@ -15,9 +31,7 @@ class UserController {
     });
 
     if (userAlreadyExists) {
-        return response.status(400).json({
-          error: "User already exists!"
-        })
+      throw new AppError("User already exists!")
     }
 
     const user = usersRepository.create({
@@ -27,6 +41,14 @@ class UserController {
     await usersRepository.save(user)
 
     return response.status(201).json(user)
+  }
+
+  async show(request: Request, response: Response) {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const all = await usersRepository.find();
+
+    return response.status(201).json(all);
   }
   
 }
